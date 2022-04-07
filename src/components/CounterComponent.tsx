07e120fc88f4c3af5bb0a3ec163d77dfe63a3951
Ritik Bardiyar <101331724+RitikBardiyar@users.noreply.github.com>
2022-04-07 15:59:08 +0530

    first commit

---
+++ src/components/CounterComponent.tsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+const CounterComponent = () => {
+  const [count, setCount] = useState(0);
+
+  const Decrement = () => {
+    if (count > 0)
+    {      
+      setCount(count - 1);
+    }
+  };
+
+
+  const Increment = () => {
+    if(count<10)
+   {
+         setCount(Number(count)+1);
+  }
+  else{
+      // alert("Hurrey, You have achieved the Target.")
+      setCount(10);
+  }
+    
+  };
+
+  const resert = (val: number) => {
+    setCount(val);
+    console.log("Resert-> " + val);
+  };
+
+  return (
+    <>
+      <div style={{}}>
+      <h1><u> Counter Component</u></h1>
+        <br></br> <br></br>
+      
+        <button onClick={() => Decrement()}>-</button>
+        <h1>{count}</h1>
+        <button onClick={() => Increment()}>+</button> 
+
+        <br></br>
+        {
+          (count==10)? <label>Hurrey, You have achieved the Target.</label>: null
+
+        }
+        <br></br> <br></br>
+        <button onClick={() => resert(0)}>Resert</button>
+      </div>
+    </>
+  );
+};
+
+export default CounterComponent;
+
